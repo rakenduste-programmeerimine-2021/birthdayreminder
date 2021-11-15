d60fbb53c562bd11d029ca069595c08f730dd453
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-15 17:08:26 +0200

    Delete temporary stuff closes #44

--- backend/src/routes/item.js
+++
@@ -1,9 +0,0 @@
-// The entire file is for testing purposes - to see if the req goes through with Postman
-
-const router = require("express").Router()
-const itemController = require("../controllers/item")
-
-router.get("/", itemController.getItems)
-router.post("/create", itemController.createItem)
-
-module.exports = router
