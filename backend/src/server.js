fe2ec7441e283faa03f144ffd2d20bbf1cf1e35f
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-15 17:08:26 +0200

    Delete temporary stuff closes #44

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
-
-// Next line is for testing purposes - to see if req goes through with Postman
-const itemRoutes = require('./routes/item');
-
 const app = express()
-
-
-// Next line is for testing purposes - to see if req goes through with Postman
 app.use(express.json());
-
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -20,10 +12,6 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-
-
-// Next line is for testing purposes - to see if req goes through with Postman
-app.use('/api/item', itemRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -35,7 +23,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`test Server started on PORT ${PORT}`))
+    app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
   })
   .catch((err) => {
     console.log(err)
