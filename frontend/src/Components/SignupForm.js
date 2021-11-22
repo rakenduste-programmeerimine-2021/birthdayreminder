595e7562241ae025038b0a715cc429aca2f24a5a
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-22 03:14:16 +0200

    FE: Sign up functionality. closes #14

--- frontend/src/Components/SignupForm.js
+++ frontend/src/Components/SignupForm.js
@@ -1,9 +1,40 @@
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, notification } from 'antd'
 
-function SignupForm(){
+function SignupForm({ addNewUserToDB }){
+    const [form] = Form.useForm();
+
+    const handleSignupForm = async (values) => {
+        const newUser = {
+            firstName: values.firstname,
+            lastName: values.lastname,
+            email: values.email,
+            password: values.password
+        }
+
+        // console.log(newUser)
+
+        if(values.password === values.confirmPassword) {
+            try {
+                await addNewUserToDB(newUser)
+                notification.success({
+                    message: 'User Created. You may Log In now :)'
+                })
+                form.resetFields()
+            } catch (err) {
+                // This will get the 1. express-validator field and message. But we have an Array...
+                // console.log(err.response.data.msg[0].param + ": " + err.response.data.msg[0].msg)
+                notification.error({
+                    message: 'Something went wrong...!'
+                })
+            }
+        } else {
+            notification.error({message:'Passwords do not match!'})
+        }
+    }
+
    return(
     <div style={{textAlign: 'center'}}>
-        <Form autoComplete='off' labelCol={{ span: 10 }} wrapperCol={{ span: 5 }}>
+        <Form form={form} autoComplete='off' labelCol={{ span: 10 }} wrapperCol={{ span: 5 }} onFinish={handleSignupForm}>
             <Form.Item label='Firstname' name='firstname' rules={[{ required: true, message:'Please insert firstname'}]}>
                 <Input />
             </Form.Item>
@@ -16,7 +47,7 @@ function SignupForm(){
             <Form.Item label='Password' name='password' rules={[{ required: true, message:'Please insert password'}]}>
                 <Input.Password />
             </Form.Item>
-            <Form.Item label='Password again' name='password1' rules={[{ required: true, message:'Please insert password again'}]}>
+            <Form.Item label='Password again' name='confirmPassword' rules={[{ required: true, message:'Please insert password again'}]}>
                 <Input.Password />
             </Form.Item>
             <Form.Item wrapperCol={{ offset: 9, span: 6 }}>
