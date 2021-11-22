a4b846efb2b32a6a4afe18f5968802efb4f45ad2
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-22 03:14:16 +0200

    FE: Sign up functionality. closes #14

--- frontend/src/Pages/SignupPage.js
+++ frontend/src/Pages/SignupPage.js
@@ -1,14 +1,25 @@
+import axios from 'axios';
+import { useHistory } from 'react-router-dom'
 import SignupForm from "../Components/SignupForm"
 import { Typography } from 'antd'
 import Header from "../Components/Header"
 const { Title } = Typography
 
 function SignupPage(){
+    const history = useHistory();
+
+    const addNewUserToDB = async (user) => {
+        const response = await axios.post('http://localhost:8082/api/auth/signup', user)
+        if(response.status === 200) {
+            history.push("/LoginPage");
+        }
+    }
+
     return(
         <div style={{ textAlign: 'center' }}>
             <Header />
             <Title style={{ marginTop: '50px' }}>Sign up!</Title>
-            <SignupForm/>
+            <SignupForm addNewUserToDB={addNewUserToDB}/>
         </div>  
     )
 }
