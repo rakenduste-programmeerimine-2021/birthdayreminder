67d75ae57d7e8ef5a5af20e1c30397a1c2462371
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-24 02:55:56 +0200

    Log Out & Header closes #9, closes #37

---
+++ frontend/src/Components/HeaderLoggedIn.js
@@ -0,0 +1,39 @@
+import { Menu, notification } from 'antd'
+import { HomeOutlined } from '@ant-design/icons'
+import { PlusCircleOutlined } from '@ant-design/icons'
+import { LogoutOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { useContext } from "react"
+import { Context } from "../store"
+import { logoutUser } from "../store/actions"
+
+function HeaderLogged(){
+  const [state, dispatch] = useContext(Context)
+
+  const handleLogOut = () => {
+    dispatch(logoutUser())
+    notification.success({
+      message:'Logged out!'
+    })
+  }
+
+  return(
+    <>
+      <Menu theme='light' mode='horizontal'>
+        <Menu.Item key='home'>
+          <Link to='/Birthdays'><HomeOutlined style={{ width:'50px'}}/></Link>
+        </Menu.Item>
+        <Menu.Item key='addBirthday' style={{ marginRight: "auto" }}>
+          <Link to='/add-birthday'><PlusCircleOutlined style={{ width:'50px'}}/></Link>
+        </Menu.Item>
+        <Menu.Item key='logout' style={{ width:'120px', textAlign: 'center'}}>
+          <Link to='/' 
+            onClick={handleLogOut}>Log out <LogoutOutlined/>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </>
+  )
+}
+
+export default HeaderLogged
