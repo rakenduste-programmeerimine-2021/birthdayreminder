dbd9d5bf7e78915612082679409ee6b4f88d0637
Anete Vaalu <55079673+anetevlu@users.noreply.github.com>
2021-11-25 21:41:38 +0200

    Add birthday form closes #20

--- frontend/src/Components/AddBirthdayForm.js
+++ frontend/src/Components/AddBirthdayForm.js
@@ -1,14 +1,14 @@
 import { Form, Input, Button, DatePicker } from 'antd'
 
 function AddBirthdayForm(){
-
-    const handleAddForm = async (values) => {        
-        console.log(values, values['birthday'].format('DD-MM-YYYY'))
-    }
+/*
+nii saab kuupäeva kätte: values['form-item-name']
+formaatida saab nt: values['form-item-name'].format('DD-MM-YYYY') 
+*/
 
     return(
         <div>
-            <Form autoComplete='off' labelCol={{ span: 10 }} wrapperCol={{ span: 5 }} onFinish={handleAddForm}>
+            <Form autoComplete='off' labelCol={{ span: 10 }} wrapperCol={{ span: 5 }}>
                 <Form.Item label='Firstname' name='firstname' rules={[{ required: true, message:'Please insert firstname'}]}>
                     <Input />
                 </Form.Item>
@@ -22,7 +22,7 @@ function AddBirthdayForm(){
                     <DatePicker />
                 </Form.Item>
                 <Form.Item wrapperCol={{ offset: 9, span: 6 }}>
-                    <Button type="primary" htmlType="submit" style={{ width: '100px'}}>
+                    <Button type="primary" htmlType="submit">
                         Add
                     </Button>
                 </Form.Item>
@@ -32,21 +32,3 @@ function AddBirthdayForm(){
 }
 
 export default AddBirthdayForm
-
-/*
-
-nii saab kuupäeva kätte: values['form-item-name']
-formaatida saab nt: values['form-item-name'].format('DD-MM-YYYY')
-
-
-<Form.Item label='Birthday' name='birthDay' rules={[{ required: true, message:'Please select birthday'}]}>
-    <Input />
-</Form.Item>
-<Form.Item label='Birthmonth' name='birthMonth' rules={[{ required: true, message:'Please select birthmont'}]}>
-    <Input />
-</Form.Item>
-<Form.Item label='Birthyear' name='birthYear' rules={[{ required: true, message:'Please select birthyear'}]}>
-    <Input />
-</Form.Item>
-
-*/
