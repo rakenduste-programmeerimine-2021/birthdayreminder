cb5c7436251113c7938f063cbaee5337d221ed27
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-30 21:06:03 +0200

    FE: Send jwt with add-birthday req closes #68
    I took the token from the global state.
    Token is also in the sessionStorage, but it is there only to copy it when the page has been refreshed by the user.

--- frontend/src/Pages/AddBirthday.js
+++ frontend/src/Pages/AddBirthday.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { useHistory } from 'react-router-dom'
+import { useContext } from 'react'
+import { Context } from '../store'
 import HeaderLoggedIn from "../Components/HeaderLoggedIn"
 import AddBirthdayForm from "../Components/AddBirthdayForm"
 import { Typography } from 'antd'
@@ -8,9 +10,15 @@ const { Title } = Typography
 
 function AddBirthday(){
     const history = useHistory()
+    const [ state, ] = useContext(Context)
+    const token = state.auth.token
 
     const addNewBirthday = async (birthday) => {
-        const res = await axios.post('http://localhost:8082/api/bday/add-birthday', birthday)
+        const res = await axios.post('http://localhost:8082/api/bday/add-birthday', birthday, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
         if(res.status === 200){
             history.push("/Birthdays")
         }
