331ba7b3a3e0ae3108930b7056c56a2aa55f5494
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-11-30 19:09:16 +0200

    BE: add-birthday functionality and jwt closes #65 closes #66
    Tested it with Postman. It works

--- backend/src/controllers/birthday.js
+++ backend/src/controllers/birthday.js
@@ -1,7 +1,14 @@
 const Birthday = require('../models/Birthday')
 
 exports.addBirthday = async (req, res) => {
-    const { firstName, lastName, email, birthDay, createdBy } = req.body
+    const { firstName, lastName, email, birthDay } = req.body
+    
+    // To make a bigger use of the jwt ... I'll take createdBy data from it
+    // Actually, jwt has already been verified. And the user info in it has been decoded.
+    // And assaigned to req.user. So here I can use that information :)
+    console.log(req.user)
+    const id = req.user.id
+    console.log(id)
 
     try {
         const newBirthday = new Birthday({
@@ -9,13 +16,14 @@ exports.addBirthday = async (req, res) => {
             lastName,
             email,
             birthDay,
-            createdBy
+            createdBy: id
         })
 
         const savedBirthday = await newBirthday.save()
         if(!savedBirthday) throw Error('Error saving birthday')
 
-        res.status(200).json({ message: 'New birthday added!'})
+        // res.status(200).json({ message: 'New birthday added!'})
+        res.status(200).send( savedBirthday )
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
--- backend/src/routes/birthday.js
+++ backend/src/routes/birthday.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const birthdayController = require('../controllers/birthday')
+const jwtAuth = require('../middleware/jwtAuth')
 
 router.get('/get-birthdays', birthdayController.getBirthdays)
-router.post('/add-birthday', birthdayController.addBirthday)
+router.post('/add-birthday', jwtAuth, birthdayController.addBirthday)
 
 module.exports = router
