11176d34e0db35fed9546c8476466a9ccaaeaa5f
Anete Vaalu <55079673+anetevlu@users.noreply.github.com>
2021-12-02 16:11:14 +0200

    BE update birthday functionality closes #28

--- backend/src/routes/birthday.js
+++ backend/src/routes/birthday.js
@@ -5,5 +5,6 @@ const jwtAuth = require('../middleware/jwtAuth')
 router.get('/get-birthdays', birthdayController.getBirthdays)
 router.post('/add-birthday', jwtAuth, birthdayController.addBirthday)
 router.delete('/delete-birthday/:id', jwtAuth, birthdayController.deleteBirthday)
+router.put('/update-birthday/:id', jwtAuth, birthdayController.updateBirthday)
 
 module.exports = router
