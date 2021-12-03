b6cb2cd319f632fa8873ee5edd44610275388c6d
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-12-03 21:17:00 +0200

    Fix App.test.js closes #89
    Before, it was giving us an error about "undefined useContext not to be a function".
    Now it doesn't anymore. And all tests are passing as they should be.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Store from './store';
 
-test('checks if birthdayreminder word exists', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/birthdayreminder/i);
-  expect(linkElement).toBeInTheDocument();
+test('checks if Siisike word does not exist', () => {
+  render(
+    <Store>
+      <App />
+    </Store>
+  );
+  const someDummyText = screen.queryByText(/Siisike/i);
+  expect(someDummyText).toBeNull();
+});
+
+test('checks if sentence exists', () => {
+  render(
+    <Store>
+      <App />
+    </Store>
+  );
+  const textThatMustBeThere = screen.getByText(/It's easy! Add the birthdays./i)
+  expect(textThatMustBeThere).toBeInTheDocument();
+
+  // One other way how to expect something to exist
+  // expect(textThatMustBeThere).not.toBeNull();
 });
