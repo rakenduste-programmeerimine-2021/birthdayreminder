4b687aed882c32f4556c02792c5b77dcbe7528e7
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-12-03 10:49:12 +0200

    FE change the way images are used and kept closes #87

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,13 +1,20 @@
 import { Typography, Button, Image } from 'antd'
 import { useHistory } from 'react-router'
+import logo from '../assets/images/logo.png'
 const { Paragraph } = Typography
+
+/* 
+    The logic behind were to keep the images and how to use them in js/css files
+    https://stackoverflow.com/questions/57111197/react-path-to-public-folder-in-css-background-image
+    https://github.com/facebook/create-react-app/issues/1248#issuecomment-266313612
+  */
 
 function HomePage(){
     const history = useHistory()
     return(
         <div className="mainDiv">
             <div className="homePageContent">
-                <Image src="./logo.png" preview={false}/>
+                <Image src={logo} preview={false}/>
                 <br />
                 <Paragraph>Do you sometimes forget the birthdays of your loved ones?</Paragraph>
                 <Paragraph>Sign up and you'll never forget them anymore!</Paragraph>
