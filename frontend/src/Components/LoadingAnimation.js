1379e3ed8e73261822600d0326a305c8cdf47b10
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-12-06 13:06:19 +0200

    Created loading component closes #101

---
+++ frontend/src/Components/LoadingAnimation.js
@@ -0,0 +1,25 @@
+import './LoadingAnimation.css';
+
+function LoadingAnimation () {
+  return (
+    // Animation code: https://loading.io/css
+    <div className="loaderArea">
+      <div className="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingAnimation;
