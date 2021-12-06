f9b34c37c7fe0b1484026cdde897439f8f7afdbf
Anete Vaalu <55079673+anetevlu@users.noreply.github.com>
2021-12-06 11:10:45 +0200

    Added view upcoming birthdays functionality closes #36

--- frontend/src/Components/UpcomingBirthdaysTable.js
+++ frontend/src/Components/UpcomingBirthdaysTable.js
@@ -1,10 +1,38 @@
 import { Table } from 'antd'
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { Context } from '../store'
 
 function UpcomingBirthdaysTable(){
     const [ state, ] = useContext(Context)
+    const [ upcomingBdays, setUpcomingBdays ] = useState([])
+    let dateToday = new Date()
+    let monthToday = dateToday.getMonth()+1 //+1 because the return values are 0-11
+    let dayToday = dateToday.getDate()
+
+    function sortOutTodaysBirthdays(){
+        let allBirthdaysFromState = []
+        let upcomingBirthdays = []
+        let fullDate
+        allBirthdaysFromState = state.birthdays.data
+        allBirthdaysFromState.forEach(element => {
+            let day, month
+            fullDate = element.birthDay.split('-')
+            day = parseInt(fullDate[0])
+            month = parseInt(fullDate[1])
+            if(month === monthToday){
+                if(day > dayToday && day <= dayToday+7){
+                    upcomingBirthdays.push(element)
+                }
+            }
+        })
+        setUpcomingBdays(upcomingBirthdays)
+    }
+
+    useEffect(() => {
+        sortOutTodaysBirthdays()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     const columns = [
         {
@@ -48,7 +76,7 @@ function UpcomingBirthdaysTable(){
                 style={{ width:'90vw', marginLeft: '5vw', marginRight:'5vw'}}
                 columns={columns}
                 rowKey='_id'
-               // dataSource={}
+                dataSource={upcomingBdays}
             />
         </>
     )
