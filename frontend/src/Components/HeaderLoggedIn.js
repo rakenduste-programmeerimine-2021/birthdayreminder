ec493422db1464f3af0bf834950c63aaf6cf004a
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-12-09 10:27:24 +0200

    Header is fixed + styled closes #115

--- frontend/src/Components/HeaderLoggedIn.js
+++ frontend/src/Components/HeaderLoggedIn.js
@@ -1,15 +1,15 @@
-import { Menu, notification } from 'antd'
-import { HomeOutlined } from '@ant-design/icons'
-import { PlusCircleOutlined } from '@ant-design/icons'
-import { LogoutOutlined } from '@ant-design/icons'
+import { Menu, notification, Image, Typography } from 'antd'
+import { HomeOutlined, LogoutOutlined, CalendarOutlined, UserAddOutlined, EditOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import { useContext } from "react"
 import { Context } from "../store"
 import { logoutUser } from "../store/actions"
+import logo from '../assets/images/logo.png'
 import './HeaderLoggedIn.css'
 
 function HeaderLoggedIn(){
   const [ , dispatch ] = useContext(Context)
+  const { Text } = Typography
 
   const handleLogOut = () => {
     dispatch(logoutUser())
@@ -22,27 +22,54 @@ function HeaderLoggedIn(){
 
   return(
     <>
-      <div className='header-menu-container'>
-        <Menu theme='light' mode='horizontal' className='header-menu-content'>
-            <Menu.Item key='home'>
-              <Link to='/birthdays'>
-                <HomeOutlined className='header-menu-icon'/>
-              </Link>
-            </Menu.Item>
-
-            <Menu.Item key='addBirthday' className='header-menu-item-on-left'>
-              <Link to='/add-birthday'>
-                <PlusCircleOutlined className='header-menu-icon'/>
-              </Link>
-            </Menu.Item>
-
-            <Menu.Item key='logout' className='header-menu-item'>
-              <Link to='/' onClick={handleLogOut}>
-                Log out <LogoutOutlined/>
-              </Link>
-            </Menu.Item>
-        </Menu>
+      {/* Duplicate route = redirects to home */}
+      <div className='logo-menu-bar'>
+        <Link to='/birthdays'>
+          <Image
+            className='app-logo-on-the-header'
+            src={logo} 
+            preview={false}
+          />
+        </Link>
       </div>
+      
+      <nav className='lower-menu-buttons-bar'>
+        <Menu theme='light' mode='horizontal'>
+          {/* Duplicate route = redirects to home */}
+          <Menu.Item key='home'>
+            <Link to='/birthdays'>
+              <HomeOutlined />
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key='addBirthday'>
+            <Link to='/add-birthday' >
+              <Text className='shown-on-bigger-screens'>Add a birthday</Text >
+              <UserAddOutlined className='shown-on-smaller-screens' />
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key='upcoming'>
+            <Link to='/upcoming'>
+              <Text className='shown-on-bigger-screens'>Upcoming birthdays</Text >
+              <CalendarOutlined className='shown-on-smaller-screens' />
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key='edit'>
+            <Link to='/all-birthdays'>
+              <Text className='shown-on-bigger-screens'>Edit birthdays</Text >
+              <EditOutlined className='shown-on-smaller-screens' />
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key='logout'>
+            <Link to='/' onClick={handleLogOut}>
+              Log out <LogoutOutlined/>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </nav>
     </>
   )
 }
