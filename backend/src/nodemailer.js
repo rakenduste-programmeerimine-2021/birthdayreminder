2a5a2d3946a27afe650d3d98039edadae7acfdbb
Anneli Põldaru <55189772+AnneliP88@users.noreply.github.com>
2021-12-29 21:43:33 +0200

    Email notifications closes #131

--- backend/src/nodemailer.js
+++ backend/src/nodemailer.js
@@ -20,3 +20,31 @@ module.exports.sendCongratsEmail = (firstname, lastname, email, senderFirstname,
                 Best wishes, ${senderFirstname} ${senderLastname}</p>`
     }).catch(error => console.log(error))
 }
+
+module.exports.sendTodaysBdaysEmail = (
+    recipientEmail,
+    recipientFirstname,
+    recipientLastname,
+    firstname,
+    lastname,
+    age
+    ) => {
+        transporter.sendMail({
+            from: 'BirthdayReminder <infobirthdayreminder@gmail.com>',
+            to: recipientEmail,
+            subject: `Birthday reminder - today is ${firstname}'s birthday`,
+            html: `<div style='text-align: center'>
+                    <img src='cid:BirthdayReminder_logo' width='200' style='margin: auto auto'/>
+                    <h2>Dear ${recipientFirstname} ${recipientLastname}!</h2>
+                    <p>Today is ${firstname} ${lastname}'s birthday!
+                        <br>
+                        ${firstname} turns ${age} today!!!
+                    </p>
+                   </div>`,
+            attachments: [{
+                filename: 'logo.png',
+                path: __dirname + '/logo.png',
+                cid: 'BirthdayReminder_logo'
+            }]
+        }).catch(error => console.log(error))
+}
